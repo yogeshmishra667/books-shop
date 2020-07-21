f0d856e8406f9d99cf8f618f65a10a3267b532c4
YoGi <yogeshmishra667@gmail.com>
2020-07-21 17:53:24 +0530

    complate crud operation for book product

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,13 +19,75 @@ const postAddProduct = async (req, res, next) => {
   return res.status(500).send({ message: ' Error in Creating Product.' });
 };
 
-//for add product
-const getProducts = (req, res, next) => {
-  res.redirect('/');
+//for get admin products
+const getProducts = async (req, res, next) => {
+  const products = await Product.find();
+  try {
+    //console.log(products);
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//admin get => admin edit products
+const getEditProduct = async (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const product = await Product.findById(req.params.productId);
+  try {
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).redirect('/');
+  }
+};
+
+//for post => admin edit products
+const postEditProduct = async (req, res, next) => {
+  const product = await Product.findById(req.body.productId);
+  try {
+    product.title = req.body.title;
+    product.price = req.body.price;
+    product.imageUrl = req.body.imageUrl;
+    product.description = req.body.description;
+    await product.save();
+
+    console.log('UPDATED PRODUCT!');
+    res.status(200).redirect('/admin/products');
+  } catch (error) {
+    console.log('something went to wrong', error);
+    return res.status(401).send(error);
+  }
+};
+//for post => delete admin product
+const postDeleteProduct = async (req, res, next) => {
+  await Product.findByIdAndRemove(req.body.productId);
+  try {
+    console.log('DESTROYED PRODUCT');
+    res.status(201).redirect('/admin/products');
+  } catch (error) {
+    res.status(401).send(error);
+    console.log(error);
+  }
 };
 
 module.exports = {
+  getProducts,
   getAddProduct,
   postAddProduct,
-  getProducts,
+  postEditProduct,
+  getEditProduct,
+  postDeleteProduct,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,11 @@ const ctrl = require('../controllers/admin');
 // /admin/add-product => GET
 router.get('/add-product', ctrl.getAddProduct);
 // /admin/products => GET
-//router.get('/products', ctrl.getProducts);
+router.get('/products', ctrl.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', ctrl.postAddProduct);
-
-//router.get('/edit-product/:productId', ctrl.getEditProduct);
-
-//router.post('/edit-product', ctrl.postEditProduct);
-
-//router.post('/delete-product', ctrl.postDeleteProduct);
+router.get('/edit-product/:productId', ctrl.getEditProduct);
+router.post('/edit-product/', ctrl.postEditProduct);
+router.post('/delete-product', ctrl.postDeleteProduct);
 
 module.exports = router;
